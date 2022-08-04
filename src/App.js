7c53f4d765329991612c21fb5e2c0601fe58764f
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-08-04 23:35:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name="Noakhali" special="Bibhag"></District>
+      <District name="BrammanBaria" special="fighter"></District>
+      <District name="Kumilla" special="Moynamoti"></District>
     </div>
   );
 }
 
+const districtStyle = {
+  backgroundColor: `lightblue`,
+  margin: `20px`,
+  borderRadius: `20px`,
+  padding: `20px`,
+};
+
+function District(props) {
+  const [power, setPower] = useState(1);
+  const boostPower = () => {
+    const newPower = power * 2;
+    setPower(newPower)
+  };
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.special}</p>
+      <h2>Power: {power}</h2>
+      <button onClick={boostPower}>Boost The Power: </button>
+    </div>
+  );
+}
 export default App;
